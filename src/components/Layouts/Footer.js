ed505fdce058be1210642dab68c1ef2a3d8f04b9
Temmy-01 <t_famakinwa@yahoo.com>
2025-03-04 12:12:01 +0100

    updated the footer

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -4,11 +4,11 @@ export default function  Footer() {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-sm-6">
-                        2024 © JustFood.
+                        2024 © Organisation Repo.
                     </div>
                     <div className="col-sm-6">
                         <div className="text-sm-end d-none d-sm-block">
-                            Design & Develop by DIGITALWEB
+                            Design & Develop by Temitope
                         </div>
                     </div>
                 </div>
